7d86d6fb2089d0155cde2063a4b99666349c0cdc
Sanderson <sandersonoficial10@hotmail.com>
2023-04-22 11:22:17 -0300

    95%

--- 17.go
+++ 17.go
@@ -6,10 +6,8 @@ func main() {
 	ele := [10]int{1, 5, 8, 12, 15, 20, 33, 50, 45, 11}
 	fmt.Println(ele)
 	soma := 0
-	for _, eles := range ele {
-		for {
-			soma += eles
-		}
+	for i := 1; i < len(ele); i += 2 {
+		soma += ele[i]
 	}
 	fmt.Println(soma)
 }
